717191a6319c1b4beb012720b6c59f93d936a8da
张帆 <zhangfan@zhangfan.local>
2019-11-08 11:24:59 +0800

    压缩html资源

--- bin/webpack.base.js
+++ bin/webpack.base.js
@@ -81,6 +81,18 @@ module.exports = {
         new HtmlWebpackPlugin({
             title:"this is title",
             template: path.join(__dirname,"../index.html"),
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            },
         })
     ]
 }
